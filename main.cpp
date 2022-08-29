0e4ca2a11873b222c1ea9dd7eb9c4cdae43b7391
vitormoreira-deb <linuxarquivos2021@gmail.com>
2022-08-29 19:01:00 -0300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,38 @@
 #include <iomanip>
 #include <locale.h>
 #include <string>
+#include <vector>
 using namespace std;
-
 struct dados{
     string nome;
     string cpf;
     int matricula;
     string endereco;
     string telefone;
-    string* email[3];
+    vector <string>email;
 };
 
 int main(){
+    bool reinicio=false;
+    do{
+        system("clear");
+        unsigned short escolha;
+        cout<<"CULTURA VIRTUAL" << endl;
+        cout<< "#######"
+        
+
+
+
+        //======Reinicip--------------
+        system("clear");
+        escolha=0;
+        cout<<"Deseja voltar ao menu principal?"<<endl<<"Digite 1 para Sim ou 2 para não: ";cin>>escolha;
+        if(escolha==1){
+            reinicio=true;
+        }
+        else{
+            reinicio=false;
+        }
+    }while(reinicio==true);
     return 0;
 }
