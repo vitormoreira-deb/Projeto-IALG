fe607bc33a93faa17cf9b16a9f7eed067f983f9d
vitormoreira-deb <linuxarquivos2021@gmail.com>
2022-08-30 01:57:36 -0300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,11 @@ using namespace std;
 struct dados{
     string nome;
     string cpf;
-    int matricula;
+    string matricula;
     string endereco;
     string telefone;
     string email;
 };
-void inserir(int &i, int &qt){
-    for(i;i<qt; i++){
-        dados user[qt];
-    }
-}
 dados* aloca(dados vet[], int &t){
     dados *novo=new dados[t+3];
     copy(vet, vet+t, novo);
@@ -27,48 +22,47 @@ int main(){
     bool reinicio=false;
     do{
         int t=3;
-        int i=0;
+        int indice=0;
         unsigned short escolha;
-        cout<<"--->> CULTURA VIRTUAL <<--- " << endl; 
-        
-        cout << "1 - Cadastrar Usuário." << endl;
-        cout << "2 - Consultar empréstimos e devoluções." << endl;
-        cout << "3 - Listar dados cadastrados ativos." << endl;
-        cout << "4 - Excluir dados." << endl;
-        cout << "5 - Efetuar empréstimo." << endl;
-        cout << "6 - Efetuar devolução." << endl;
-        cout << "7 - Exportar contas para arquivo binário." << endl;
-        cout << "8 - Sair." << endl;
+        cout<<" -------------->> BIBLIOTECA CULTURA VIRTUAL <<------------" << endl; 
+        cout << "|| 1 - Cadastrar Usuário                                  ||"<< endl;
+        cout << "|| 2 - Consultar empréstimos e devoluções                 ||" << endl;
+        cout << "|| 3 - Listar dados cadastrados ativos                    ||" << endl;
+        cout << "|| 4 - Excluir dados                                      ||" << endl;
+        cout << "|| 5 - Efetuar empréstimo                                 ||"<< endl;
+        cout << "|| 6 - Efetuar devolução                                  ||" << endl;
+        cout << "|| 7 - Exportar contas para arquivo binário               ||"<< endl;
+        cout << "|| 8 - Sair                                               ||" << endl;
+        cout << "----------------------------------------------------------"<<endl;
         
         cout<<"--->> CULTURA VIRTUAL version: 1.0 <<---" << endl; 
-        cin >> escolha;
+        cout<<"Insira um valor do menu: ";cin >> escolha;
+        system("clear");
         dados *cadastro=new dados[t];
         switch(escolha){
             case 1:
             bool inserir;
             do{
-                if(i<(t)){
-                    cout<<"Insira nome: ";cin>>cadastro[i].nome;
-                    cout<<"Insira CPF: ";cin>>cadastro[i].cpf;
-                    cout<<"Insira matricula: ";cin>>cadastro[i].matricula;
-                    cout<<"Insira endereço: ";cin>>cadastro[i].endereco;
-                    cout<<"Insira telefone: ";cin>>cadastro[i].telefone;
-                    cout<<"Insira e-mail: ";cin>>cadastro[i].email;
-                    while(cadastro.email.size()>0){
-                        //continua 
+                if(indice<(t)){
+                    char sn;
+                    cout<<"Insira nome: ";cin.ignore();getline(cin,cadastro[indice].nome);
+                    cout<<"Insira CPF: ";cin>>cadastro[indice].cpf;
+                    cout<<"Insira matricula: ";cin>>cadastro[indice].matricula;
+                    cout<<"Insira endereço: ";cin.ignore();getline(cin,cadastro[indice].endereco);
+                    cout<<"Insira telefone: ";cin.ignore();getline(cin,cadastro[indice].telefone);
+                    cout<<"Insira e-mail: ";cin.ignore();getline(cin,cadastro[indice].email);
+                    system("clear");
+                    cout<<"Deseja cadastrar novo usuário? 1-sim 2-nao ";cin>>sn;
+                    if(sn=='1'){
+                        indice++;
+                    }
+                    else{
+                        break;
                     }
                 }
                 else{
-                    char valores;
-                    cout<<"Armazenamento cheio!!!. Posso aumentar o armazenamento em mais 3 valores?"<<endl<<"1-Sim, 2-Não    "cin>>valores;
-
-                    if(valores=='1'){
-                        cadastro=aloca(cadastro, t);
-                        inserir=true;
-                    }
-                    else{
-                        inserir=false;
-                    }
+                    cadastro=aloca(cadastro, t);
+                    inserir=true;
                 }
             }while(inserir=true);
             break;
@@ -102,5 +96,6 @@ int main(){
             reinicio=false;
         }
     }while(reinicio==true);
+    system("clear;");
     return 0;
 }
